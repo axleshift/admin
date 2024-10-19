b72359be5e495e154702602f5e5666a099fa599c
Arcka1n <128115665+Arcka1n@users.noreply.github.com>
2024-10-20 07:52:02 +0800

    initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,37 @@
-import React, { Suspense, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { CSpinner, useColorModes } from '@coreui/react';
-import './scss/style.scss';
+import React, { Suspense, useEffect } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { CSpinner, useColorModes } from '@coreui/react'
+import './scss/style.scss'
 
-const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
-const Login = React.lazy(() => import('./views/pages/login/Login'));
-const Register = React.lazy(() => import('./views/pages/register/Register'));
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
-const Forgotpass = React.lazy(()=> import('./views/pages/profile/forgotpass'))
-const Resetpass = React.lazy(()=> import('./views/pages/profile/resetpass'))
+const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
+const Login = React.lazy(() => import('./views/pages/login/Login'))
+const Register = React.lazy(() => import('./views/pages/register/Register'))
+const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
+const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+const Forgotpass = React.lazy(() => import('./views/pages/profile/forgotpass'))
+const Resetpass = React.lazy(() => import('./views/pages/profile/resetpass'))
 
 const App = () => {
-  const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme');
-  const storedTheme = useSelector((state) => state.changeState.theme);
+  const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
+  const storedTheme = useSelector((state) => state.changeState.theme)
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.href.split('?')[1]);
-    const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0];
+    const urlParams = new URLSearchParams(window.location.href.split('?')[1])
+    const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
     if (theme) {
-      setColorMode(theme);
+      setColorMode(theme)
     }
 
     if (isColorModeSet()) {
-      return;
+      return
     }
 
-    setColorMode(storedTheme);
-  }, []);
+    setColorMode(storedTheme)
+  }, [])
 
   return (
     <BrowserRouter>
-     
       <Suspense
         fallback={
           <div className="pt-3 text-center">
@@ -51,7 +50,7 @@ const App = () => {
         </Routes>
       </Suspense>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
