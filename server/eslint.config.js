d885ff049ecdd6ed1152f38f56a9a0521bc01b93
Arcka1n <128115665+Arcka1n@users.noreply.github.com>
2024-10-20 14:03:27 +0800

    initial

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,9 +1,23 @@
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically picks the version you have installed
+      },
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      // Add any specific ESLint rules you need here
+    },
+  },
   pluginReact.configs.flat.recommended,
 ];
