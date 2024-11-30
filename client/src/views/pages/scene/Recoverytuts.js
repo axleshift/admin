eeec5bf0637e86e5de9008ca2bd5e631f92c355d
Arcka1n <128115665+Arcka1n@users.noreply.github.com>
2024-11-30 18:59:44 +0800

    initia

--- client/src/views/pages/scene/Recoverytuts.js
+++ client/src/views/pages/scene/Recoverytuts.js
@@ -40,8 +40,8 @@ const RecoveryTutorial = () => {
       </CCardHeader>
       <CCardBody>
         <ol>
-          <li>Click "Backup Now" to create a database backup.</li>
-          <li>Enter a backup timestamp, filename, and database name below, then click "Recover Now" to restore a backup.</li>
+          <li>Click &quot;Backup Now&quot; to create a database backup.</li>
+          <li>Enter a backup timestamp, filename, and database name below, then click &quot;Recover Now&quot; to restore a backup.</li>
         </ol>
         <CFormInput
           type="text"
