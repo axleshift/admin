44ca8db3c42a8ef6e9bc2d19accb86c6ef02615d
Arcka1n <128115665+Arcka1n@users.noreply.github.com>
2025-03-13 22:29:22 +0800

    try'

--- client/src/views/pages/scene/button.js
+++ client/src/views/pages/scene/button.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import { CButton } from '@coreui/react';
 import { useSelector } from 'react-redux';
-import { logActivity } from '../../../utils/logActivity';
 
 const ExampleButtonPage = () => {
   const user = useSelector(state => state.auth?.user); 
 
-  const handleClick = () => {
-    if (user) {
-      logActivity({
-        userId: user.id,
-        name: user.name,
-        role: user.role,
-        department: user.department,
-        activity: 'Clicked the button'
-      });
-    }
-  };
+
 
   return (
     <div>
-      <CButton onClick={handleClick}>Click me</CButton>
+      <CButton>Click me</CButton>
     </div>
   );
 };
