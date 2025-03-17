95809f4162fb0b9fec9649d6d6dd0c983c2af0ec
freight-capstone <freightadmi@gmail.com>
2025-03-17 06:50:07 +0000

    Build server

--- assets/financialanalytics--LevJwM-.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-oNzC6x8M.js";import"./vendor-react-B6URQkMn.js";import"./vendor-redux-DipIbRRz.js";import"./vendor-coreui-BGyit7oL.js";const o=()=>i.jsx("div",{children:"financialanalytics\\"});export{o as default};
