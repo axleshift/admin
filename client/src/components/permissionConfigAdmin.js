c98aba067b2b835fe4288591ebafb0d12cac475f
Arcka1n <128115665+Arcka1n@users.noreply.github.com>
2025-03-17 11:21:33 +0800

    init

--- client/src/components/permissionConfigAdmin.js
+++ client/src/components/permissionConfigAdmin.js
@@ -1,11 +1,11 @@
 // permissionConfigAdmin.js
 const accessPermissions = {
     admin: {
-      HR: ['/hrdash', '/worker', '/jobposting', '/payroll', '/profile', '/Settings', '/changepass', '/request', '/freight/transaction'],
-      Core: ['/coredash', '/shipment', '/customer', '/monthly', '/daily', '/breakdown', '/profile', '/Settings', '/changepass'],
-      Finance: ['/financedash', '/freight/transaction', '/oversales', '/freightaudit', '/financialanalytics', '/invoice', '/profile', '/Settings', '/changepass'],
-      Logistics: ['/logisticdash', '/shipment', '/customer', '/monthly', '/daily', '/breakdown', '/logistic1/index', '/logistic1/pin', '/profile', '/Settings', '/changepass'],
-      Administrative: ['/employeedash', '/useractivity/index', '/restore', '/registernew', '/PendingRequest', '/AccessReview', '/profile', '/Settings', '/changepass', '/cron']
+      HR: ['/hrdash', '/worker', '/jobposting', '/payroll', '/freight/transaction'],
+      Core: ['/coredash', '/shipment', '/customer', '/monthly', '/daily', '/breakdown'],
+      Finance: ['/financedash', '/freight/transaction', '/oversales', '/freightaudit', '/financialanalytics', '/invoice'],
+      Logistics: ['/logisticdash', '/shipment', '/customer', '/monthly', '/daily', '/breakdown', '/logistic1/index', '/logistic1/pin'],
+      Administrative: ['/employeedash', '/restore', '/registernew', '/PendingRequest', '/AccessReview',]
     }
   };
   
--- client/src/components/permissionConfigManager.js
+++ client/src/components/permissionConfigManager.js
@@ -1,11 +1,11 @@
 // permissionConfigManager.js
 const accessPermissions = {
     Manager: {
-      HR: ['/hrdash', '/worker', '/jobposting', '/profile', '/Settings', '/changepass'],
-      Core: ['/coredash', '/customer', '/monthly', '/daily', '/breakdown', '/shipment', '/profile', '/Settings', '/changepass'],
-      Finance: ['/financedash', '/freight/transaction', '/oversales', '/freightaudit', '/financialanalytics', '/profile', '/Settings', '/changepass'],
-      Logistic: ['/logisticdash', '/logistic1/index', '/logistic1/pin', '/profile', '/Settings', '/changepass'],
-      Administrative: ['/employeedash', '/useractivity/index', '/profile', '/Settings', '/changepass']
+      HR: ['/hrdash', '/worker', '/jobposting'],
+      Core: ['/coredash', '/customer', '/monthly', '/daily', '/breakdown', '/shipment',],
+      Finance: ['/financedash', '/freight/transaction', '/oversales', '/freightaudit', '/financialanalytics',],
+      Logistic: ['/logisticdash', '/logistic1/index', '/logistic1/pin',],
+      Administrative: ['/employeedash', '/useractivity/index',]
     }
   };
   
