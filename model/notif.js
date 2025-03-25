316a4c332980befec93087b157c95cb5e438cbcc
freight-capstone <freightadmi@gmail.com>
2025-03-25 02:37:37 +0000

    Build server

---
+++ model/notif.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const NotificationSchema =new mongoose.Schema({
+  message: { type: String, required: true },
+  read: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Notification = mongoose.model("Notification", NotificationSchema);
+export default Notification;
