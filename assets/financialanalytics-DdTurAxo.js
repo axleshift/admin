a4c03736008b015d34383377ccbe207d47edada5
freight-capstone <freightadmi@gmail.com>
2025-03-27 05:25:44 +0000

    Build client

---
+++ assets/financialanalytics-DdTurAxo.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-BWhqHrvX.js";import"./vendor-react-B6URQkMn.js";import"./vendor-redux-DipIbRRz.js";import"./vendor-coreui-BBLjqpko.js";const o=()=>i.jsx("div",{children:"financialanalytics\\"});export{o as default};
