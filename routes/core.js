4c7d10ff27350deb79bd32c66228a6ec989f7e7a
freight-capstone <freightadmi@gmail.com>
2025-03-27 01:02:04 +0000

    Build client

--- routes/core.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { fetchCore1Data } from '../controllers/core.js';
-
-const router = express.Router();
-
-router.get('/fetch-core', fetchCore1Data);
-
-export default router;
