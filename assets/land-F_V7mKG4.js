e8f3edff6df602728af1625dc0748b2d8f841130
freight-capstone <freightadmi@gmail.com>
2025-04-04 06:45:34 +0000

    Build client

---
+++ assets/land-F_V7mKG4.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-SoqsusEQ.js";import"./vendor-react-B6URQkMn.js";import"./vendor-redux-DipIbRRz.js";import"./vendor-coreui-DHT2NCDT.js";const m=()=>r.jsx("div",{children:"LandFreight"});export{m as default};
--- routes/notification.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getnotif, addnotif } from '../controllers/notification.js';
-
-const router = express.Router();
-
-router.get('/getnotif', getnotif);
-router.post('/postnotif', addnotif);
-
-export default router;
