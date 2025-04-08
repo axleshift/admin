0160cbf39b47fe4e2cbecca9d6d6361ab5786fa6
freight-capstone <freightadmi@gmail.com>
2025-04-08 03:06:23 +0000

    Build client

---
+++ assets/SecurityCheck-BcjtHb6j.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D3aMJk9e.js";import"./vendor-react-B6URQkMn.js";import"./vendor-redux-DipIbRRz.js";import"./vendor-coreui-BDhfDOiu.js";const m=()=>r.jsx("div",{children:"AI?"});export{m as default};
