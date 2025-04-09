4ae1a98000ec293c5a4d283f1f1f726d8d989aed
freight-capstone <freightadmi@gmail.com>
2025-04-09 09:31:17 +0000

    Build client

--- UTIL/resetFailedattempts.js
+++
@@ -1,9 +0,0 @@
-import User from "../model/User.js";
-
-export const resetFailedAttempts = async (userId) => {
-    try {
-        await User.updateOne({ _id: userId }, { failedAttempts: 0 });
-    } catch (error) {
-        console.error("Error resetting failed attempts:", error);
-    }
-};
