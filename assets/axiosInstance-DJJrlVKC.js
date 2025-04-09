1812370c9a39b275645cc0cec8fcb34670531adb
freight-capstone <freightadmi@gmail.com>
2025-04-09 01:44:13 +0000

    Build client

---
+++ assets/axiosInstance-DJJrlVKC.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index-BM2XebS9.js";const t=a.create({baseURL:"https://backend-admin.axleshift.com",withCredentials:!0,headers:{"Content-Type":"application/json"}});export{t as a};
