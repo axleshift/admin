c0bafb5f8b9d1a0780e18027c578ba9b97fd0796
freight-capstone <freightadmi@gmail.com>
2025-04-10 05:09:08 +0000

    Build client

--- routes/logistics.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import {log1vehicle} from "../controllers/logistics.js";
-const router = express.Router();
-
-router.get('/vehicle', log1vehicle)
-export default router;
