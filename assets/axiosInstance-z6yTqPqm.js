5b107839d838e9f025f560f16942360c88f0c941
freight-capstone <freightadmi@gmail.com>
2025-04-11 15:03:47 +0000

    Build client

---
+++ assets/axiosInstance-z6yTqPqm.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index-Creopves.js";const t=a.create({baseURL:"https://backend-admin.axleshift.com",withCredentials:!0,headers:{"Content-Type":"application/json"}});export{t as a};
