75292bcd06ca96bccd90526ea116ea1f920ea5c2
freight-capstone <freightadmi@gmail.com>
2025-04-20 08:13:45 +0000

    Build client

---
+++ assets/land-BNksaKVV.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DLAH96kV.js";import"./vendor-react-B6URQkMn.js";import"./vendor-redux-C7peqcPR.js";import"./vendor-coreui-Be7TXQEF.js";const m=()=>r.jsx("div",{children:"LandFreight"});export{m as default};
