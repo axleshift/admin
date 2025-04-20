dc74abd48c9ca35b74951b7939e0464df08b4d9c
Arcka1n <128115665+Arcka1n@users.noreply.github.com>
2025-04-20 14:37:09 +0800

    ng

--- server/index.js
+++ server/index.js
@@ -50,13 +50,14 @@ const app = express();
 const server = http.createServer(app);
 
 // ✅ 5. Middleware
+app.use(helmet());
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser()); 
-app.use(helmet());
 app.use(morgan("common"));
 app.use(bodyParser.json());
 
-app.use(cors())
+
 
 // Create uploads directory if it doesn't exist
 const uploadsDir = path.join(__dirname, 'uploads');
