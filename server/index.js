7a39a303147d2506e01517bb54987134601c94a7
Arcka1n <128115665+Arcka1n@users.noreply.github.com>
2025-04-20 14:29:21 +0800

    anak

--- server/index.js
+++ server/index.js
@@ -56,10 +56,7 @@ app.use(helmet());
 app.use(morgan("common"));
 app.use(bodyParser.json());
 
-app.use(cors({
-  origin: 'https://admin.axleshift.com',  // Or use an array for multiple origins
-  credentials: true  // This allows cookies/credentials to be sent
-}));
+app.use(cors())
 
 // Create uploads directory if it doesn't exist
 const uploadsDir = path.join(__dirname, 'uploads');
