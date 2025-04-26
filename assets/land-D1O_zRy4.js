4dd1ec38017b5382dea3f1e34bc17dedca8f9ed1
freight-capstone <freightadmi@gmail.com>
2025-04-26 05:04:29 +0000

    Build server

--- assets/land-D1O_zRy4.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-3FTAhQcu.js";import"./vendor-react-BHxUanFt.js";import"./vendor-redux-CSji-Y9p.js";import"./vendor-coreui-DaIzHuu_.js";const m=()=>r.jsx("div",{children:"LandFreight"});export{m as default};
---
+++ model/audit.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+// Define a schema for each item
+const itemSchema = new mongoose.Schema({
+  name: { type: String },
+  quantity: { type: Number },
+  price: { type: Number },
+  total: { type: Number },
+});
+
+const invoiceSchema = mongoose.Schema(
+  {
+    invoiceNumber: { type: String },
+    trackingId: { type: String, default: "12" },
+    firstName: { type: String },
+    customerId: { type: String },
+    lastName: { type: String },
+    email: { type: String },
+    phone: { type: String },
+    deliveryDate: { type: Date },
+    discounts: { type: Number },
+    dueDate: { type: Date },
+    address: { type: String },
+    products: { type: [itemSchema], required: true },
+    selectedCurrency: { type: String },
+    status: {
+      type: String,
+      default: "Pending",
+    },
+    totalAmount: { type: Number },
+    paymentMethod: { type: String },
+    qrCode: { type: String },
+    notes: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const invoiceModel = mongoose.model("Invoice", invoiceSchema);
+
+export default invoiceModel;
