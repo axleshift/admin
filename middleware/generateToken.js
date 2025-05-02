8a9708a71465a8b34527207acc13a39c26c86412
freight-capstone <freightadmi@gmail.com>
2025-05-02 22:17:49 +0000

    Build client

--- middleware/generateToken.js
+++
@@ -1,9 +0,0 @@
-// filepath: c:\Users\ryans\OneDrive\Desktop\withsecurity\admin - Copy\server\middleware\generateToken.js
-import jwt from 'jsonwebtoken';
-
-export const generateAccessToken = (user) => 
-    jwt.sign(user, process.env.ACCESS_SECRET, { expiresIn: "15m" });
-
-export const generateRefreshToken = (user) => 
-    jwt.sign(user, process.env.REFRESH_SECRET, { expiresIn: "7d" });
-
