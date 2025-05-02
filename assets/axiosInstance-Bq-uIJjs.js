8d497fbfe231a6d7ac98ba7c814f45391a96d868
freight-capstone <freightadmi@gmail.com>
2025-05-02 07:26:52 +0000

    Build client

---
+++ assets/axiosInstance-Bq-uIJjs.js
@@ -0,0 +1,1 @@
+import{a}from"./index-xsH4HHeE.js";const t=a.create({baseURL:"https://backend-admin.axleshift.com",withCredentials:!0,headers:{"Content-Type":"application/json"}});export{t as a};
---
+++ assets/land-Btl_7XGJ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C1kKK9j-.js";import"./vendor-react-BHxUanFt.js";import"./vendor-redux-CSji-Y9p.js";import"./vendor-coreui-CLDxaGvm.js";const m=()=>r.jsx("div",{children:"LandFreight"});export{m as default};
